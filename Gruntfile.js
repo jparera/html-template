e83e7e4fcaf74e189b5d188dc5a2ead39e31ac00
Joan Parera <joan@warelet.com>
2013-09-08 21:33:16 +0200

    Validate javascript files with JSHint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,112 @@
 module.exports = function(grunt) {
-	var cfgDir = "cfg/",
-		srcDir = "src/",
-		outDir = "dist/",
-		cfg = {
-			watch: {
-				options: {
-					livereload: true
-				},
-				html: {
-					files: outDir + "*.html"
-				},
-				js: {
-					files: outDir + "js/**/*.js"
-				},
-				css: {
-					files: outDir + "css/**/*.css"
-				},
-				dstImages: {
-					files: outDir + "img/**/*.{png,jpg,gif}"
-				},
-				srcImages: {
-					files: srcDir + "img/**/*.{png,jpg,gif}",
-					tasks: ["copy:img"],
-					options: {
-						livereload: false
-					}
-				},
-				jade: {
-					files: [srcDir + "*.jade", cfgDir + "jade*"],
-					tasks: ["jade"],
-					options: {
-						livereload: false
-					}
-				},
-				compass: {
-					files: [srcDir + "sass/**/*.{scss,sass}"],
-					tasks: ["copy:sass", "compass"],
-					options: {
-						livereload: false
-					}
-				}
-			},
-			copy: {
-				img: {
-					expand: true,
-					cwd: srcDir,
-					src: "img/**/*.{png,jpg,gif}",
-					dest: outDir
-				},
-				sass: {
-					expand: true,
-					cwd: srcDir,
-					src: "sass/**/*.{scss,sass}",
-					dest: outDir
-				}
-			},
-			jade: {
-				dist: {
-					options: {
-						data: grunt.file
-							.readJSON(cfgDir + "jade-data.json")
-					},
-					expand: true,
-					cwd: srcDir,
-					src: "*.jade",
-					dest: outDir,
-					ext: ".html"
-				}
-			},
-			compass: {
-				dist: {
-					options: {
-						cssDir: outDir + "css",
-						sassDir: outDir + "sass",
-						imagesDir: outDir + "img",
-						environment: "production"
-					}
-				}
-			}
-		};
+    var cfgDir = "cfg/",
+        srcDir = "src/",
+        outDir = "dist/",
+        cfg = {
+            watch: {
+                options: {
+                    livereload: true
+                },
+                html: {
+                    files: outDir + "*.html"
+                },
+                js: {
+                    files: outDir + "js/**/*.js"
+                },
+                css: {
+                    files: outDir + "css/**/*.css"
+                },
+                dstImages: {
+                    files: outDir + "img/**/*.{png,jpg,gif}"
+                },
+                srcImages: {
+                    files: srcDir + "img/**/*.{png,jpg,gif}",
+                    tasks: ["copy:img"],
+                    options: {
+                        livereload: false
+                    }
+                },
+                jade: {
+                    files: [
+                        srcDir + "*.jade",
+                        cfgDir + "jade*"
+                    ],
+                    tasks: ["jade"],
+                    options: {
+                        livereload: false
+                    }
+                },
+                compass: {
+                    files: [srcDir + "sass/**/*.{scss,sass}"],
+                    tasks: ["copy:sass", "compass"],
+                    options: {
+                        livereload: false
+                    }
+                }
+            },
+            copy: {
+                img: {
+                    expand: true,
+                    cwd: srcDir,
+                    src: "img/**/*.{png,jpg,gif}",
+                    dest: outDir
+                },
+                sass: {
+                    expand: true,
+                    cwd: srcDir,
+                    src: "sass/**/*.{scss,sass}",
+                    dest: outDir
+                }
+            },
+            jade: {
+                dist: {
+                    options: {
+                        data: grunt.file
+                            .readJSON(cfgDir + "jade-data.json")
+                    },
+                    expand: true,
+                    cwd: srcDir,
+                    src: "*.jade",
+                    dest: outDir,
+                    ext: ".html"
+                }
+            },
+            compass: {
+                dist: {
+                    options: {
+                        cssDir: outDir + "css",
+                        sassDir: outDir + "sass",
+                        imagesDir: outDir + "img",
+                        environment: "production"
+                    }
+                }
+            },
+            jshint: {
+                src: {
+                    options: {
+                        jshintrc: srcDir + ".jshintrc"
+                    },
+                    src: [srcDir + "js/**/*.js"]
+                },
+                node: {
+                    options: {
+                        jshintrc: ".jshintrc"
+                    },
+                    src: ["*.js"]
+                }
+            }
+        };
 
-	grunt.initConfig(cfg);
+    grunt.initConfig(cfg);
 
-	grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.loadNpmTasks("grunt-contrib-jade");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-contrib-compass");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-jade");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-compass");
 
-	grunt.registerTask("default", ["copy", "jade", "compass"]);
-	grunt.registerTask("live", ["default", "watch"]);
+    grunt.registerTask("default", ["lint", "compile"]);
+    grunt.registerTask("lint", ["jshint"]);
+    grunt.registerTask("compile", ["copy", "jade", "compass"]);
+    grunt.registerTask("live", ["default", "watch"]);
 };
